a5ae080a0d4c6c50d5ad840607db1abfd50218cc
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-03-21 15:11:27 -0400

    Initial commit for p4

--- bral-Python/project4/crawler.py
+++ bral-Python/project4/crawler.py
@@ -1,12 +1,14 @@
 import socket
 import bs4
 import sys
+import time
 
 
+#FUTURE_USERNAME = sys.argv[1]
+#FUTURE_PW = sys.argv[2]
 
-FUTURE_USERNAME = sys.argv[1]
-FUTURE_PW = sys.argv[2]
-
+USERNAME = "001655997"
+PW = "ONO8WGDJ"
 
 
 class Crawler:
@@ -23,16 +25,41 @@ class Crawler:
     def send_fb_get(self, page):
         self.sock.send("GET /{}/ HTTP/1.1\nHost: fring.ccs.neu.edu\n\n".format(page).encode())
         self.curr_page = self.sock.recv(4096).decode("utf-8")
-        return self.curr_page[8:12]
+        self.get_cookie(self.curr_page)
+        return self.curr_page#[9:12]
 
-    def login():
+    def login(self, username, pw):
+        request = str("POST /accounts/login/ HTTP/1.1\n" +
+                       "Host: fring.ccs.neu.edu\n" +
+                       "Connection: keep-alive\n" +
+                       "Cache-Control: max-age=0\n" +
+                       "Content-Type: application/x-www-form-urlencoded\n" +
+                       "Cookie: " + self.cookie + "\n" +
+                       "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\n" +
+                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:52.0) Gecko/20100101 Firefox/52.0\n" +
+                       "Accept-Encoding: gzip, deflate\n" +
+                       "username={}&password={}\n\n".format(USERNAME, PW))
+        self.sock.send(request.encode())
+
+
+        for i in range(5):
+            print(self.sock.recv(4096))
+            time.sleep(3)
+
+    def get_cookie(self, res):
+        csrf = res[res.find("csrftoken="):]
+        csrf = csrf[:csrf.find(";")]
+        session = res[res.find("sessionid="):]
+        session = session[:session.find(";")]
+        self.cookie = "{};{}".format(csrf, session)
 
 
 
 
 def main():
     crawl = Crawler()
-    print(crawl.send_fb_get("accounts/login/?next=/fakebook"))
+    crawl.send_fb_get("accounts/login/?next=/fakebook")
+    print(crawl.login(USERNAME, PW))
 
 
 
