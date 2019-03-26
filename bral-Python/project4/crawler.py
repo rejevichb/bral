d2a1e2cd06e752d7e0a3ba5bce9d1244db2b205c
Jameson O'Connor <oconnor.jam@husky.neu.edu>
2019-03-26 19:05:11 -0400

    officially broke it:( ... we should finish this on Wednesday

--- bral-Python/project4/crawler.py
+++ bral-Python/project4/crawler.py
@@ -15,7 +15,7 @@ PW = "ONO8WGDJ"
 class Crawler:
 
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = None
         self.cookie = None
         self.seen = set()
         self.fringe = set()
@@ -23,11 +23,20 @@ class Crawler:
         self.csrf = None
         self.session = None
 
+
+    # need to have a socket for each one So make this service each request
+    def request(self, msg):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(("fring.ccs.neu.edu", 80))
+        self.sock.send(msg.encode("utf-8"))
+        return self.sock.recv(4096).rstrip().decode()
+
 
     def send_fb_get(self, page):
         if self.cookie is None:
-            self.sock.send("GET /{}/ HTTP/1.1\r\nHost: fring.ccs.neu.edu\r\n\r\n".format(page).encode())
+
+            self.request("GET /{}/ HTTP/1.1\nHost: fring.ccs.neu.edu\r\n".format(page))
+            #self.sock.send("GET /{}/ HTTP/1.1\nHost: fring.ccs.neu.edu\r\n".format(page).encode())
             self.curr_page = self.sock.recv(4096).decode("utf-8")
             self.get_cookie()
             #print("Curr Page: {}".format(self.curr_page))
@@ -38,10 +47,22 @@ class Crawler:
                            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
                            "Connection: keep-alive\r\n" +
                            "Cookie: {}\r\n\r\n").format(page, self.cookie)
+
+            newmsg = str("GET /{}/ HTTP/1.1\r\n" +
+                        "Host: fring.ccs.neu.edu\r\n" +
+                        "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0\r\n" +
+                        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
+                        "Accept-Language: en-US,en;q=0.5\r\n" +
+                        "Accept-Encoding: gzip, deflate\r\n" +
+                        "Referer: http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/\r\n" +
+                        "Upgrade-Insecure-Requests: 1\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Cookie: {}\r\n\r\n").format(page, self.cookie)
+            print(msg)
             self.sock.send(msg.encode())
             acc = ""
             for i in range(5):
-                acc += self.sock.recv(4096).decode("utf-8")
+                acc += self.sock.recv(4096).decode()
 
             #print(acc)
 
@@ -57,15 +78,15 @@ class Crawler:
         print("FRINGE : {}".format(self.fringe))
 
     def login(self, username, pw):
-        request = str("POST /accounts/login/ HTTP/1.1\r\n" +
-                             "Host: fring.ccs.neu.edu\r\n" +
-                             "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0\r\n" +
-                             "Content-Length: 109\r\n" +
-                             "Connection: Keep-Alive\r\n" +
-                             "Cookie: " + self.cookie + "\r\n" +
-                             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n" +
-                             "Content-Type: application/x-www-form-urlencoded\r\n\r\n" +
-                             "username={}&password={}&csrfmiddlewaretoken={}&next=/fakebook/\r\n".format(USERNAME, PW, self.csrf))
+        request = str("POST /accounts/login/ HTTP/1.1\n" +
+                             "Host: fring.ccs.neu.edu\n" +
+                             "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0\n" +
+                             "Content-Length: 109\n" +
+                             "Connection: Keep-Alive\n" +
+                             "Cookie: " + self.cookie + "\n" +
+                             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n" +
+                             "Content-Type: application/x-www-form-urlencoded\n\n" +
+                             "username={}&password={}&csrfmiddlewaretoken={}&next=%2Ffakebook%2F\r\n".format(USERNAME, PW, self.csrf))
 
         self.sock.send(request.encode())
         acc = ""
@@ -74,7 +95,7 @@ class Crawler:
 
         self.get_cookie()
         self.curr_page = acc
-        # self.get_new_session()
+        self.get_new_session()
         return acc
 
     def get_cookie(self):
@@ -88,11 +109,11 @@ class Crawler:
         self.csrf = csrf
         self.session = session[session.find("=") + 1:]
 
-    # def get_new_session(self):
-    #     res = self.curr_page
-    #     session = res[res.find("sessionid="):]
-    #     session = session[:session.find(";")]
-    #     self.cookie = "csrftoken={}; {}".format(self.csrf, session)
+    def get_new_session(self):
+        res = self.curr_page
+        session = res[res.find("sessionid="):]
+        session = session[:session.find(";")]
+        self.cookie = "csrftoken={}; {}".format(self.csrf, session)
 
 
 def main():
